33bbf2d9957d6cb8af22c63842da056cd957f40e
SmolinPavel <isingwithaz@gmail.com>
2022-02-19 12:49:56 +0300

    ReactDOM render

--- index.tsx
+++ index.tsx
@@ -14,6 +14,34 @@ const React = {
   },
 };
 
+const ReactDOM = {
+  render: (element, container) => {
+    if (typeof element === "string" || typeof element === "number") {
+      container.appendChild(document.createTextNode(String(element)));
+
+      return;
+    }
+
+    const { props, tag } = element;
+    const domElement = document.createElement(tag);
+
+    if (props.children) {
+      for (const child of props.children) {
+        ReactDOM.render(child, domElement);
+      }
+    }
+
+    for (const prop in props) {
+      const value = props[prop];
+      if (prop !== "children") {
+        domElement[prop] = value;
+      }
+    }
+
+    container.appendChild(domElement);
+  },
+};
+
 const App = () => (
   <div>
     <header>Header</header>
@@ -25,4 +53,4 @@ const App = () => (
   </div>
 );
 
-console.log(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
