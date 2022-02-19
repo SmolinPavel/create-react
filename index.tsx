6b571342a8e42563a708fa0ccb214dca65c4cc00
SmolinPavel <isingwithaz@gmail.com>
2022-02-19 12:42:49 +0300

    JSX & React Elements

--- index.tsx
+++ index.tsx
@@ -1,1 +1,28 @@
-console.log("hello react");
+const React = {
+  createElement: (tag, props, ...children) => {
+    if (typeof tag === "function") {
+      return tag({ ...props, children });
+    }
+
+    return {
+      tag,
+      props: {
+        ...props,
+        children,
+      },
+    };
+  },
+};
+
+const App = () => (
+  <div>
+    <header>Header</header>
+    <main>
+      <h1>Page title</h1>
+      <p>lorem...</p>
+    </main>
+    <footer>Footer</footer>
+  </div>
+);
+
+console.log(<App />);
